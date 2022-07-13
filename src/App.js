8546141ea8af8a47375852677aea6bf77fe03083
FatahChan <Ahmadfatllah89@gmail.com>
2022-07-13 13:12:48 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter, Switch, Route, Redirect} from "react-router-dom";
+import HeaderComponent from "./Compnents/Header/Header.Component";
+import ProductListingPageComponent from "./Compnents/PLP/PLP.Component";
+import ProductDetailsPageComponent from "./Compnents/PDP/ProductDetailsPage.Component";
+import CartComponent from "./Compnents/Cart/Cart.Component";
+import "./App.css"
+import {getCategories} from "./BackendCalls/getCategories";
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      cart: [],
+      selectedCurrency: {label: "USD", symbol: "$"},
+      categories: []
+    }
+  }
+  componentDidMount() {
+    getCategories().then((res) => {
+      this.setState({categories: res})
+    })
+    if(sessionStorage.getItem("cart")){
+      this.setState({cart: JSON.parse(sessionStorage.getItem("cart"))})
+    }
+  }
+
+  setCart(cart){
+    console.log("setCart", cart);
+    this.setState({cart: cart});
+    sessionStorage.setItem("cart", JSON.stringify(cart));
+  }
+  getCart() {
+    return JSON.parse(JSON.stringify(this.state.cart));
+  }
+  getSelectedCurrency() {
+    return this.state.selectedCurrency;
+  }
+  setSelectedCurrency(currencyLabel){
+    this.setState({selectedCurrency: currencyLabel})
+  }
+  render() {
+    return (
+        <div>
+          <BrowserRouter>
+            <HeaderComponent
+                key={JSON.stringify({ selectedCurrency: this.state.selectedCurrency})}
+                setSelectedCurrency={this.setSelectedCurrency.bind(this)}
+                getSelectedCurrency={this.getSelectedCurrency.bind(this)}
+                setCart={this.setCart.bind(this)}
+                getCart={this.getCart.bind(this)}
+            />
+            <Switch>
+              <Route exact path="/" >
+                <Redirect to={`/default`}/>
+              </Route>
+              <Route path="/product/:id" render={(props) => (
+                  <ProductDetailsPageComponent
+                      key={JSON.stringify({id: props.match.params.id, currency: this.state.selectedCurrency})}
+                      setCart={this.setCart.bind(this)}
+                      getCart={this.getCart.bind(this)}
+                      getSelectedCurrency={this.getSelectedCurrency.bind(this)}/>
+                 )}
+              />
+              <Route exact path="/cart" render={() => (
+                 <CartComponent
+                     key={JSON.stringify({cart: this.state.cart, currency: this.state.selectedCurrency})}
+                     setCart={this.setCart.bind(this)}
+                     getCart={this.getCart.bind(this)}
+                     getSelectedCurrency={this.getSelectedCurrency.bind(this)}/>
+                  )}
+                />)
+              }
+              />
+              <Route exact path="/:category" render={(props) => (
+                <ProductListingPageComponent
+                  key={JSON.stringify({id: props.match.params.category, currency: this.state.selectedCurrency})}
+
+                  setSelectedCurrency={this.setSelectedCurrency.bind(this)}
+                  getSelectedCurrency={this.getSelectedCurrency.bind(this)}
+                  setCart={this.setCart.bind(this)}
+                  getCart={this.getCart.bind(this)}
+                />)}
+              />
+            </Switch>
+          </BrowserRouter>
+        </div>
+    );
+  }
 }
 
 export default App;
