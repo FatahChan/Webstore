bb69cd45394b0171aaa2b33690c5ff24f3a98ea1
FatahChan <Ahmadfatllah89@gmail.com>
2022-07-21 21:14:51 +0200

    Feedback fix
    Used fetch instead of apollo for category fetch

--- src/BackendCalls/ApolloClient.js
+++ src/BackendCalls/ApolloClient.js
@@ -1,6 +1,16 @@
 import {ApolloClient, InMemoryCache} from "@apollo/client";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    AttributeSet: {
+      // Singleton types that have no identifying field can use an empty
+      // array for their keyFields.
+      keyFields: [],
+    },
+  },
+});
+
 export const client = new ApolloClient({
   uri: 'http://localhost:4000/',
-  cache: new InMemoryCache(),
+  cache: cache,
 });
